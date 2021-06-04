e477a703412af2550ac33587eec3dd11bb1e62e0
daohiep22 <85162681+daohiep22@users.noreply.github.com>
2021-06-04 10:06:21 +0700

    Update checkout.blade.php

--- resources/views/client/checkout.blade.php
+++ resources/views/client/checkout.blade.php
@@ -61,7 +61,7 @@
                 <div class="row">
                     <div class="col-sm-4">
                         <div class="logo pull-left">
-                            <a href="/"><img src="images/home/logo.png" alt="" /></a>
+                            <a href="/"><img src="toulonslogo.png" alt="" /></a>
                         </div>
                     </div>
                     <div class="col-sm-8">
